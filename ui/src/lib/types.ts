a840e3498445e9575a57b5d47348ee2c9af4be7c
Nirav Joshi <niravjoshi3000@gmail.com>
2025-10-16 16:53:31 +0530

    added type for pda

--- ui/src/lib/types.ts
+++ ui/src/lib/types.ts
@@ -1,10 +1,22 @@
 import type { Idl } from "@coral-xyz/anchor";
 
+export type PDASeed = {
+  kind: string;
+  value?: number[];
+  path?: string;
+  account?: string;
+};
+
+export type AccountPDADetails = {
+  seeds: PDASeed[];
+};
+
 export type IdlInstruction = Idl["instructions"][number];
 export type ModIdlAccount = IdlInstruction["accounts"][number] & {
   writable?: boolean;
   signer?: boolean;
   address?: string;
+  pda?: AccountPDADetails;
 };
 
 export interface SavedAccount {
